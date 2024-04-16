275eb1f650b650010514e2df2c423d1f9aeb7bd4
Tsukanov Denis <tsukanovdenis83@yandex.ru>
2024-04-16 21:02:24 +0400

    add styles

--- src/components/Day/Day.tsx
+++ src/components/Day/Day.tsx
@@ -13,12 +13,13 @@ interface DayProps {
 const Day: FC<DayProps> = ({daysArr, changedDate, numberOfTasks}) => {
 
     const bgClass = daysArr.date.getTime() === new Date(new Date().setHours(0, 0, 0, 0)).getTime() ? 'today' : '';
+    const bgRedClass = numberOfTasks ? 'red-round' : ''
 
-    const cangeClass = daysArr.date.getTime() === new Date(changedDate.setHours(0, 0, 0, 0)).getTime() ? 'changed' : '';
+    const cangeClass = daysArr.date.getTime() === new Date(changedDate.setHours(0, 0, 0, 0)).getTime() ? 'changed-border' : '';
     return(
         <div className={`day ${daysArr.status} ${bgClass} ${cangeClass}`} id={daysArr.id}>
             {`${daysArr.date.getDate()}`}
-            <div>{numberOfTasks ? numberOfTasks : ''}</div>
+            <div className={`day-numbers ${bgRedClass}`}>{numberOfTasks ? numberOfTasks : ''}</div>
         </div>
     )
 }
